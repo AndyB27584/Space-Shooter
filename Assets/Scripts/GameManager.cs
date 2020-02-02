1345c66b542401e34c1fb9974dfdcf53aa8810b9
Andrew Baloglow <andybalo@Andrews-MacBook-Pro-2.local>
2020-02-02 15:22:14 +1100

    Testing again...

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,31 +6,37 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private bool _isGameOver = false;
-    [SerializeField]
-    private bool _isCoopMode = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R) && _isGameOver)
         {
             SceneManager.LoadScene(1);
         }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
     }
 
     public void GameOver()
     {
         _isGameOver = true;
+    }
+
+    public void PauseGame()
+    {
+            Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+            Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,6 +11,9 @@ public class UIManager : MonoBehaviour
     private Sprite[] _livesSprites;
     private Image _livesImage;
     private Text _gameOverText, _restartText;
+    private GameManager _gameManager;
+    private GameObject _pauseMenu;
+    private bool _isGamePaused = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +22,27 @@ public class UIManager : MonoBehaviour
         _livesImage = transform.GetChild(1).GetComponent<Image>();
         _gameOverText = transform.GetChild(2).GetComponent<Text>();
         _restartText = transform.GetChild(3).GetComponent<Text>();
+        _pauseMenu = transform.GetChild(4).gameObject;
 
         _livesImage.sprite = _livesSprites[3];//int lives
         _gameOverText.enabled = false;
         _restartText.enabled = false;
+        _pauseMenu.SetActive(false);
+
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+        if (_gameManager == null)
+        {
+            Debug.LogError("Game Manager is NULL");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu();
+        }
     }
 
     public void UpdateScoreText(int score)
@@ -59,6 +73,22 @@ public class UIManager : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
             _gameOverText.enabled = false;
             yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void PauseMenu()
+    {
+        if (!_isGamePaused)
+        {
+            _gameManager.PauseGame();
+            _pauseMenu.SetActive(true);
+            _isGamePaused = true;
+        }
+        else
+        {
+            _gameManager.UnpauseGame();
+            _pauseMenu.SetActive(false);
+            _isGamePaused = false;
         }
     }
 }
